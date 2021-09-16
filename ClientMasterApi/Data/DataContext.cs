875251f1faac91eec1e356fc4aac0aec1d5d8ebf
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-09-16 05:33:45 +0300

    Create DataContext.

---
+++ ClientMasterApi/Data/DataContext.cs
@@ -0,0 +1,57 @@
+﻿using System.Data.Entity;
+using System.Data.SQLite;
+using Core.Models;
+
+
+namespace ClientMasterApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base(new SQLiteConnection()
+        {
+            ConnectionString = new SQLiteConnectionStringBuilder()
+            {
+                DataSource = ".\\ClientMaster.db", ForeignKeys = true
+            }.ConnectionString
+        }, true)
+        {
+
+        }
+
+        public DbSet<Client> Clients { get; set; }
+
+        protected override void OnModelCreating (DbModelBuilder modelBuilder)
+        {
+            ConfigureEntityPrimaryKeys(modelBuilder);
+            ConfigureEntityProperties(modelBuilder);
+        }
+
+        private static void ConfigureEntityPrimaryKeys(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Client>()
+                .HasKey(c => c.Id);
+        }
+
+        private static void ConfigureEntityProperties(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Client>()
+                .Property(c => c.Forename)
+                .HasMaxLength(56)
+                .IsRequired();
+
+            modelBuilder.Entity<Client>()
+                .Property(c => c.Surname)
+                .HasMaxLength(56)
+                .IsRequired();
+
+            modelBuilder.Entity<Client>()
+                .Property(c => c.Age)
+                .IsRequired();
+
+            modelBuilder.Entity<Client>()
+                .Property(c => c.Comment)
+                .HasMaxLength(224);
+        }
+    }
+    
+}
