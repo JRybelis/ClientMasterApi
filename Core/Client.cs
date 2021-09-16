0cb96f6336e957f2957de88e17900f8826507a39
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-09-16 04:19:34 +0300

    Create Core project, register dependency. Set client entity up.

---
+++ Core/Client.cs
@@ -0,0 +1,11 @@
+﻿namespace Core
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Forename { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Comment { get; set; }
+    }
+}
